69313cb56af5a180f0144bb74c5cfae466cfd22b
kanat <kanat@eub.com>
2021-10-02 19:53:29 +0600

    CrimeListFragment viewmodel

---
+++ app/src/main/java/com/kanat/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,28 @@
+package com.kanat.criminalintent
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+
+private const val TAG = "CrimeListFragment"
+
+class CrimeListFragment : Fragment() {
+
+    private val crimeListViewModel by lazy {
+        ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "Total crimes ${crimeListViewModel.crimes.size}")
+    }
+
+    companion object {
+        fun newInstance(): CrimeListFragment {
+            return CrimeListFragment()
+        }
+    }
+
+}
