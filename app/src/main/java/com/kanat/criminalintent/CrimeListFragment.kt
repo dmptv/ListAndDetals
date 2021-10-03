2edcae996a0e679c8f1f15b9366997468994e185
kanat <kanat@eub.com>
2021-10-03 11:56:44 +0600

    recycler view add

--- app/src/main/java/com/kanat/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/kanat/criminalintent/CrimeListFragment.kt
@@ -1,13 +1,21 @@
 package com.kanat.criminalintent
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
+
+    private lateinit var crimeRecyclerView: RecyclerView
 
     private val crimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
@@ -17,6 +25,19 @@ class CrimeListFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         Log.d(TAG, "Total crimes ${crimeListViewModel.crimes.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+
+        crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        return view
     }
 
     companion object {
