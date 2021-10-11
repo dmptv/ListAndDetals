56d04739220b4ba99cdc8bf34fd34e75052f6810
kanat <kanat@eub.com>
2021-10-11 23:31:52 +0600

    dao , repo, aplication

---
+++ app/src/main/java/com/kanat/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,16 @@
+package com.kanat.criminalintent.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.kanat.criminalintent.Crime
+import java.util.*
+
+@Dao
+interface CrimeDao {
+
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): List<Crime>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): Crime
+}
--- app/src/main/java/com/kanat/criminalintent/database/CrimeDatabase.kt
+++ app/src/main/java/com/kanat/criminalintent/database/CrimeDatabase.kt
@@ -7,4 +7,6 @@ import com.kanat.criminalintent.Crime
 
 @Database(entities = [Crime::class], version = 1)
 @TypeConverters(CrimeTypeConverters::class)
-abstract class CrimeDatabase: RoomDatabase()
+abstract class CrimeDatabase: RoomDatabase() {
+    abstract fun crimeDao(): CrimeDao
+}
